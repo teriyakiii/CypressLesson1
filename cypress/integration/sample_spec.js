73d05deb880212ea7eea5dbbb1afbe0e2cd5102b
Victor Yao <ywj@Victors-Air.fritz.box>
2021-08-15 20:08:25 +1200

    new file

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+const { it } = require("date-fns/locale")
+const { isExportDeclaration } = require("typescript")
+
+describe ('first assignment', function(){
+    it('does addition', function(){
+        exptect(10+15).to.equal(25)
+    })
+})
